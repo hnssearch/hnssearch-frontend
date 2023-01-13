ed2a7b37ce94ee56ada508005059b9e83a026c4b
AndiR16 <andir16@gmx.ch>
2023-01-13 05:12:02 +0100

    Adds tailwindcss

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <div>
+      <header className="flex w-full p-3 justify-between">
+        <div className="flex space-x-3 items-center">
+          <Link to="/dwebpulse">dwebpulse</Link>
+        </div>
+        <div className="flex space-x-3 items-center">
+          <Link to="/About">About</Link>
+          <Link to="/Settings">Settings</Link>
+        </div>
+      </header>
+    </div>
+  );
 }
 
 export default HomePage;
