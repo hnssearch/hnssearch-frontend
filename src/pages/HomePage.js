5d3389da5bbf5dcd4aa8015888219721a76e6700
AndiR16 <andir16@gmx.ch>
2023-01-13 20:51:02 +0100

    Adds additional responsive behaviour

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -4,7 +4,7 @@ import logo from "../assets/images/logo.png";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 function HomePage() {
   return (
-    <div className="bg-neutral-100 h-screen dark:bg-neutral-900">
+    <div className="bg-neutral-100 h-screen dark:bg-neutral-900 max-h-screen">
       <header className="flex w-full p-3 justify-between text-gray-800 text-lg dark:text-gray-300">
         <div className="flex space-x-3 items-center">
           <Link className="link" to="/dwebpulse">
@@ -21,7 +21,11 @@ function HomePage() {
         </div>
       </header>
       <form className="flex flex-col flex-grow items-center mt-52">
-        <img className="h-40 dark:invert" src={logo} alt="hnssearch_logo" />
+        <img
+          className="h-40 dark:invert scale-50 sm:scale-75 md:scale-75 lg:scale-100 object-bottom"
+          src={logo}
+          alt="hnssearch_logo"
+        />
         <div
           className="flex w-full mt-10 max-w-sm rounded-full border border-gray-300 items-center pl-7 pr-2 py-4
         sm:max-w-xl lg:max-w-2xl focus-within:shadow-sm hover:shadow-sm"
@@ -31,6 +35,14 @@ function HomePage() {
             type="text"
           />
           <MagnifyingGlassIcon className="h-6 mr-3 text-gray-800 dark:text-gray-300" />
+        </div>
+        <div>
+          <button
+            className="bg-gray-200 p-3 px-5 mt-5 rounded-full border border-gray-300 text-gray-800 text-md
+            ring-gray-400 hover:ring-1 focus:outline-none hover:shadow-md"
+          >
+            Search
+          </button>
         </div>
       </form>
     </div>
