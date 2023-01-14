be80591505c3d8d773bf6f9232e2ebddc459cec6
AndiR16 <andir16@gmx.ch>
2023-01-14 07:28:51 +0100

    Adds url routing with search term - format: /search?s=

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useRef } from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
 import Footer from "../components/Footer";
+import { useNavigate } from "react-router-dom";
 function HomePage() {
+  const searchInputRef = useRef(null);
+  const navigate = useNavigate();
+  const search = (e) => {
+    e.preventDefault();
+    const searchTerm = searchInputRef.current.value;
+
+    if (!searchTerm) return;
+
+    navigate(`/search?s=${searchTerm}`);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center bg-neutral-100 h-screen dark:bg-neutral-900">
       <header className="flex w-full p-3 justify-between text-gray-800 text-lg dark:text-gray-300">
@@ -30,6 +43,7 @@ function HomePage() {
           <input
             className="flex-grow focus:outline-none text-xl dark:text-gray-300 bg-neutral-100 dark:bg-neutral-900"
             type="text"
+            ref={searchInputRef}
           />
           <MagnifyingGlassIcon className="h-6 mr-3 text-gray-800 dark:text-gray-300" />
         </div>
@@ -37,6 +51,8 @@ function HomePage() {
           <button
             className="bg-gray-200 p-3 px-5 mt-5 rounded-full border border-gray-300 text-gray-800 text-md
             ring-gray-400 hover:ring-1 focus:outline-none hover:shadow-md"
+            onClick={search}
+            type="submit"
           >
             Search
           </button>
