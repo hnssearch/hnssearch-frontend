371c14b333fdae35772996db1fba3186183fee28
AndiR16 <andir16@gmx.ch>
2023-01-14 06:43:53 +0100

    Adds footer and tweaks responsiveness

---
+++ src/components/Footer.js
@@ -0,0 +1,45 @@
+function Footer() {
+  return (
+    <footer
+      className="text-sm bg-gray-200 text-gray-800 flex w-full p-2 justify-between text-gray-800 text-lg
+        dark:text-gray-300 dark:bg-neutral-800"
+    >
+      <div className="px-8 py-3 flex space-x-3 items-center">
+        <p>&copy; 2023 HNSSearch</p>
+      </div>
+      <div className="flex">
+        <ul className="flex space-x-3 items-center">
+          <li>
+            <a
+              href="https://hnssearch.medium.com/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Blog
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/hnssearch"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Twitter
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/HNSSearch"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/images/logo.png";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/solid";
+import Footer from "../components/Footer";
 function HomePage() {
   return (
-    <div className="bg-neutral-100 h-screen dark:bg-neutral-900 max-h-screen">
+    <div className="flex flex-col items-center justify-center bg-neutral-100 h-screen dark:bg-neutral-900">
       <header className="flex w-full p-3 justify-between text-gray-800 text-lg dark:text-gray-300">
         <div className="flex space-x-3 items-center">
           <Link className="link" to="/dwebpulse">
@@ -20,12 +21,8 @@ function HomePage() {
           </Link>
         </div>
       </header>
-      <form className="flex flex-col flex-grow items-center mt-52">
-        <img
-          className="h-40 dark:invert scale-50 sm:scale-75 md:scale-75 lg:scale-100 object-bottom"
-          src={logo}
-          alt="hnssearch_logo"
-        />
+      <form className="flex flex-col flex-grow items-center mt-52 w-4/5">
+        <img className="h-32 dark:invert" src={logo} alt="hnssearch_logo" />
         <div
           className="flex w-full mt-10 max-w-sm rounded-full border border-gray-300 items-center pl-7 pr-2 py-4
         sm:max-w-xl lg:max-w-2xl focus-within:shadow-sm hover:shadow-sm"
@@ -45,6 +42,7 @@ function HomePage() {
           </button>
         </div>
       </form>
+      <Footer />
     </div>
   );
 }
