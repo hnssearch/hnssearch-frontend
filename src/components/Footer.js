0879da5810cc2325dec1e1270b54cc304d0e9f0a
AndiR16 <andir16@gmx.ch>
2023-01-21 22:24:49 +0100

    Fixes footer issues on mobile

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 function Footer() {
   return (
     <footer
-      className="text-sm bg-gray-200 text-gray-800 flex flex-wrap w-full p-2 justify-between text-gray-800 text-lg
-        dark:text-gray-300 dark:bg-neutral-800"
+      className="text-sm bg-neutral-300 text-gray-800 flex flex-wrap w-full p-2 justify-center md:justify-between
+      text-gray-800 text-lg dark:text-gray-300 dark:bg-neutral-800"
     >
       <div className="px-8 py-3 flex space-x-3 items-center">
         <p>&copy; 2023 HNSSearch</p>
