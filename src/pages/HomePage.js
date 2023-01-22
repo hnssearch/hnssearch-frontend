a2929183a99b95497a637a17e030e65249ceec38
AndiR16 <andir16@gmx.ch>
2023-01-22 10:25:48 +0100

    Fixes #1 - centers form in middle of screen on mobile

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -34,33 +34,39 @@ function HomePage() {
           </Link>
         </div>
       </header>
-      <form className="flex flex-col flex-grow items-center mt-52 w-4/5">
-        <img className="h-32 dark:invert" src={logo} alt="hnssearch_logo" />
-        <div
-          className="flex w-full mt-10 max-w-sm rounded-full border border-gray-300 items-center pl-7 pr-2 py-4
+      <div className="flex flex-col flex-grow w-full items-center">
+        <form className="flex flex-col items-center mt-auto mb-auto md:mt-52 md:mb-0 w-4/5">
+          <img
+            className="h-20 md:h-32 dark:invert"
+            src={logo}
+            alt="hnssearch_logo"
+          />
+          <div
+            className="flex w-full mt-10 max-w-sm rounded-full border border-gray-300 items-center pl-7 pr-2 py-4
         sm:max-w-xl lg:max-w-2xl focus-within:shadow-sm hover:shadow-sm"
-        >
-          <input
-            className="flex-grow focus:outline-none text-xl dark:text-gray-300 bg-neutral-100 dark:bg-neutral-900"
-            type="text"
-            ref={searchInputRef}
-          />
-          <MagnifyingGlassIcon
-            className="h-6 mr-3 text-gray-800 dark:text-gray-300 cursor-pointer"
-            onClick={search}
-          />
-        </div>
-        <div>
-          <button
-            className="bg-gray-200 p-3 px-5 mt-5 rounded-full border border-gray-300 text-gray-800 text-md
-            ring-gray-400 hover:ring-1 focus:outline-none hover:shadow-md"
-            onClick={search}
-            type="submit"
           >
-            Search
-          </button>
-        </div>
-      </form>
+            <input
+              className="flex-grow focus:outline-none text-xl dark:text-gray-300 bg-neutral-100 dark:bg-neutral-900"
+              type="text"
+              ref={searchInputRef}
+            />
+            <MagnifyingGlassIcon
+              className="h-6 mr-3 text-gray-800 dark:text-gray-300 cursor-pointer"
+              onClick={search}
+            />
+          </div>
+          <div>
+            <button
+              className="bg-gray-200 p-3 px-5 mt-5 rounded-full border border-gray-300 text-gray-800 text-md
+            ring-gray-400 hover:ring-1 focus:outline-none hover:shadow-md"
+              onClick={search}
+              type="submit"
+            >
+              Search
+            </button>
+          </div>
+        </form>
+      </div>
       <Footer />
     </div>
   );
