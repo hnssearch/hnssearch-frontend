4d40ba637beb0ca79cb176b98fc78f3bc179cf86
AndiR16 <andir16@gmx.ch>
2023-01-25 22:02:16 +0100

    Redirects non-existent urls to homepage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,9 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ResultsPage from "./pages/ResultsPage";
 import AboutPage from "./pages/AboutPage";
@@ -11,6 +16,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/search" element={<ResultsPage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </Router>
     </div>
