65dc7ce506959d675ac5a38d7456bfadf567bb24
AndiR16 <andir16@gmx.ch>
2023-03-03 12:44:21 +0100

    Adds styling to stats page

--- src/components/StatTile.js
+++ src/components/StatTile.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function StatTile({ title, value }) {
   return (
-    <div className="flex items-center justify-center bg-white rounded-lg shadow-md p-4 text-center h-48">
-      <div className="flex flex-col justify-center">
+    <div className="flex items-center justify-center bg-white rounded-lg shadow-md p-4 text-center h-48 dark:bg-neutral-700">
+      <div className="flex flex-col justify-center text-gray-700 dark:text-neutral-200">
         <p className="text-4xl font-bold">{value}</p>
         <h2 className="text-lg font-medium mt-5 mb-2">{title}</h2>
       </div>
--- src/pages/StatsPage.js
+++ src/pages/StatsPage.js
@@ -4,7 +4,7 @@ import stats from "../assets/files/stats.json";
 // Implementation of stats currently with static file and import; will be changed to fetch at a later point.
 function StatsPage() {
   return (
-    <div className="flex flex-col justify-center h-screen py-8 px-4">
+    <div className="flex flex-col justify-center py-8 px-4 bg-neutral-100 h-screen dark:bg-neutral-900">
       <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
         <StatTile title="Pages" value={stats.n_entries} />
         <StatTile title="Different Sites" value={stats.n_domains} />
