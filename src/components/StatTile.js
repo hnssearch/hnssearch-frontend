46aa2ea7ba51d9ff3d7c3b73b850f7db31701cae
AndiR16 <andir16@gmx.ch>
2023-03-04 17:48:34 +0100

    Finalizes basic stats page

--- src/components/StatTile.js
+++ src/components/StatTile.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function StatTile({ title, value }) {
   return (
-    <div className="flex items-center justify-center bg-white rounded-lg shadow-md p-4 text-center h-48 dark:bg-neutral-700">
+    <div className="md:h-48 flex items-center justify-center mb-10 bg-white rounded-lg shadow-md p-4 text-center dark:bg-neutral-700">
       <div className="flex flex-col justify-center text-gray-700 dark:text-neutral-200">
         <p className="text-4xl font-bold">{value}</p>
         <h2 className="text-lg font-medium mt-5 mb-2">{title}</h2>
