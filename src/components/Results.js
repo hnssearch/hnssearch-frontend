5e30a69f1d15a4c564dd37067d3b79489b63bbb5
AndiR16 <andir16@gmx.ch>
2023-03-09 22:31:28 +0100

    Adds https verification UI elements on search results

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,6 +4,7 @@ import { MeiliSearch } from "meilisearch";
 import { useNavigate } from "react-router-dom";
 import Pagination from "./Pagination";
 import HandypediaInfobox from "./HandypediaInfobox";
+import protect_icon from "../assets/images/protect.png"
 
 // public key for search, free to use
 const client = new MeiliSearch({
@@ -100,9 +101,13 @@ function Results({ query, page }) {
           >
             <div>
               <div>
-                <p className="text-sm text-gray-700 dark:text-neutral-200">
+                <div className="flex items-center">
+                  {/* comparison needs to be changed for production to check if ssl is True or False and URL to docs needs to be changed*/}
+                  {item.ssl === "" ? null :  (<a href="https://docs.hnssearch.io/handshake/"> <img className="h-4 float-left mr-1" src={protect_icon} alt="protect_icon" title="verified https available"/></a>)}
+                <p className="flex text-sm text-gray-700 dark:text-neutral-200">
                   {item.url_no_dot}
                 </p>
+                </div>
                 <a
                   className="text-lg text-blue-800 font-bold dark:text-blue-400"
                   href={item.url_no_dot}
