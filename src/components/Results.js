6d8940d3fa2cefd509a04c99ba0a296cee7ad7aa
AndiR16 <andir16@gmx.ch>
2023-04-26 20:51:11 +0200

    Changes way urls are displayed. Not full path anymore but divided by >

--- src/components/Results.js
+++ src/components/Results.js
@@ -4,6 +4,7 @@ import { MeiliSearch } from "meilisearch";
 import { useNavigate } from "react-router-dom";
 import Pagination from "./Pagination";
 import HandypediaInfobox from "./HandypediaInfobox";
+import CustomLink from "../utils/CustomLink";
 
 // public key for search, free to use
 const client = new MeiliSearch({
@@ -17,7 +18,7 @@ const clientHandypedia = new MeiliSearch({
 });
 
 const index = client.index("sites");
-const indexHandypedia = clientHandypedia.index("handypedia")
+const indexHandypedia = clientHandypedia.index("handypedia");
 
 const fetchData = async (query, page) => {
   const response = await index.search(query, {
@@ -68,57 +69,63 @@ function Results({ query, page }) {
 
   useEffect(() => {
     fetchDataHandypedia(query)
-        .then((res) => {
-          // console.log(res);
-          setHandypediaResults(res.hits);
-        })
-        .catch((e) => {
-          console.log(e.message);
-        });
+      .then((res) => {
+        // console.log(res);
+        setHandypediaResults(res.hits);
+      })
+      .catch((e) => {
+        console.log(e.message);
+      });
   }, [query]);
 
   return (
     <div className="max-w-7xl">
       <div className="ml-28 lg:ml-4 lg:float-right mr-10 lg:max-w-xs mt-3 hidden md:block">
-        <HandypediaInfobox handypediaResults={handypediaResults}/>
+        <HandypediaInfobox handypediaResults={handypediaResults} />
       </div>
-    <div className="flex flex-col mt-3">
-      {/*<h2 className="mb-3">React HTTP Request with Async Await Example</h2>*/}
-      {resultHits.length !== 0 ? null : (
-        <div className="ml-28 mr-10 max-w-4xl">
-          <p className="text-gray-700 dark:text-neutral-200">
-            No search results
-          </p>
-        </div>
-      )}
-      {resultHits.map((item, idx) => {
-        return (
-          <div
-            className="px-5 py-2 mb-3 md:mb-0 md:py-5 md:ml-28 md:mr-10 md:max-w-4xl break-words bg-neutral-100
+      <div className="flex flex-col mt-3">
+        {/*<h2 className="mb-3">React HTTP Request with Async Await Example</h2>*/}
+        {resultHits.length !== 0 ? null : (
+          <div className="ml-28 mr-10 max-w-4xl">
+            <p className="text-gray-700 dark:text-neutral-200">
+              No search results
+            </p>
+          </div>
+        )}
+        {resultHits.map((item, idx) => {
+          return (
+            <div
+              className="px-5 py-2 mb-3 md:mb-0 md:py-5 md:ml-28 md:mr-10 md:max-w-4xl break-words bg-neutral-100
              dark:bg-neutral-700"
-            key={idx}
-          >
-            <div>
+              key={idx}
+            >
               <div>
-                <p className="text-sm text-gray-700 dark:text-neutral-200">
-                  {item.url_no_dot}
-                </p>
-                <a
-                  className="text-lg text-blue-800 font-bold dark:text-blue-400"
-                  href={item.url_no_dot}
-                >
-                  {item.title}
-                </a>
-                <p className="text-gray-700 dark:text-neutral-200">
-                  {item.description?.length > 25 ? item.description.substring(0, 300) : item.content.substring(0, 300)}
-                  {(item.description?.length > 25 && item.description?.length > 300) || ((item.description === null || item.description?.length <= 25) && item.content.length > 300) ? '...' : ''}
-                </p>
+                <div>
+                  <CustomLink url={item.url_no_dot} />
+                  <a
+                    className="text-lg text-blue-800 font-bold dark:text-blue-400"
+                    href={item.url_no_dot}
+                  >
+                    {item.title}
+                  </a>
+                  <p className="text-gray-700 dark:text-neutral-200">
+                    {item.description?.length > 25
+                      ? item.description.substring(0, 300)
+                      : item.content.substring(0, 300)}
+                    {(item.description?.length > 25 &&
+                      item.description?.length > 300) ||
+                    ((item.description === null ||
+                      item.description?.length <= 25) &&
+                      item.content.length > 300)
+                      ? "..."
+                      : ""}
+                  </p>
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
       <div className="flex flex-col mt-auto mb-5 w-full p-3 justify-between items-center text-gray-700 dark:text-neutral-200">
         <Pagination query={query} page={page} totalPages={results.totalPages} />
       </div>
